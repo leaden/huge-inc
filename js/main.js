df5241b440530009566275f5fa3889962b8f62bf
Maribel Diaz <maribel.diaz.j@gmail.com>
2017-05-11 21:53:10 -0300

    codigo completo pero no funcionando

--- js/main.js
+++ js/main.js
@@ -1,8 +1,28 @@
 document.getElementById("toggle").addEventListener("click", function(event){
 	event.preventDefault();
-	document.getElementById("nav-header").classList.toggle("open");
+	document.getElementById("header").classList.toggle("open");
 	document.getElementById("body").classList.toggle("overflow-hidden");
 
 });
 
-var currentScroll = window.pageYOffset || document.body.scrollTop;
+
+var lastScrollTop = 0;
+   window.addEventListener("scroll", function(){
+   	var currentScroll = window.pageYOffset || document.body.scrollTop;
+   	if (currentScroll > lastScrollTop){
+   		console.log("down");
+   	}else {
+   		console.log("up");
+   	}
+   	lastScrollTop = currentScroll;
+   }, false);
+
+   if (currentScroll > lastScrollTop){
+   	document.getElementById("nav-header").classList.remove("header-in");
+   	document.getElementById("nav-header").style.opacity = 0;
+   	
+   } else{
+
+   }
+
+   
